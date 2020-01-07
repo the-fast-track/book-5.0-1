9fb173640909aa26553b285dcbe6b6da56e04c9b
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:23:16 +0100

    Add 26.1: Exposing an API for Conferences

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
