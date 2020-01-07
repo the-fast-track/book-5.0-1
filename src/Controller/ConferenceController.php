157983412d00a4b361d3a450a0a2323b53a6d56e
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:19:35 +0100

    Add 21.3: Purging the HTTP Cache for Testing

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -44,6 +44,19 @@ class ConferenceController extends AbstractController
     }
 
     /**
+     * @Route("/conference_header", name="conference_header")
+     */
+    public function conferenceHeader(ConferenceRepository $conferenceRepository)
+    {
+        $response = new Response($this->twig->render('conference/header.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+        $response->setSharedMaxAge(3600);
+
+        return $response;
+    }
+
+    /**
      * @Route("/conference/{slug}", name="conference")
      */
     public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir)
