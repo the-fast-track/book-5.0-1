07eb6bc93be9c2a2094aa8f06fd9d9f96f40761e
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:17:27 +0100

    Add 18.4: Going Async for Real

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -27,7 +27,9 @@ class CommentRepository extends ServiceEntityRepository
     {
         $query = $this->createQueryBuilder('c')
             ->andWhere('c.conference = :conference')
+            ->andWhere('c.state = :state')
             ->setParameter('conference', $conference)
+            ->setParameter('state', 'published')
             ->orderBy('c.createdAt', 'DESC')
             ->setMaxResults(self::PAGINATOR_PER_PAGE)
             ->setFirstResult($offset)
