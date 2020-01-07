9da6c2eba81a1e4e4ab4911cc402a6259ff8276b
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:21:24 +0100

    Add 23.1: Adding a new Step in the Workflow

---
+++ src/ImageOptimizer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
+
+class ImageOptimizer
+{
+    private const MAX_WIDTH = 200;
+    private const MAX_HEIGHT = 150;
+
+    private $imagine;
+
+    public function __construct()
+    {
+        $this->imagine = new Imagine();
+    }
+
+    public function resize(string $filename): void
+    {
+        list($iwidth, $iheight) = getimagesize($filename);
+        $ratio = $iwidth / $iheight;
+        $width = self::MAX_WIDTH;
+        $height = self::MAX_HEIGHT;
+        if ($width / $height > $ratio) {
+            $width = $height * $ratio;
+        } else {
+            $height = $width / $ratio;
+        }
+
+        $photo = $this->imagine->open($filename);
+        $photo->resize(new Box($width, $height))->save($filename);
+    }
+}
