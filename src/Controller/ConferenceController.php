cda0e4ee88f997cef6896a428fc62f384b5e8e7d
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:19:35 +0100

    Add 21.1: Activating the Symfony HTTP Cache Kernel

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -35,9 +35,12 @@ class ConferenceController extends AbstractController
      */
     public function index(ConferenceRepository $conferenceRepository)
     {
-        return new Response($this->twig->render('conference/index.html.twig', [
+        $response = new Response($this->twig->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]));
+        $response->setSharedMaxAge(3600);
+
+        return $response;
     }
 
     /**
