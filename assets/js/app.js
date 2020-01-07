48a832bfcfbcf9c5894d4b93150a978f13e91af8
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:20:41 +0100

    Add 22.1: Leveraging Bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
