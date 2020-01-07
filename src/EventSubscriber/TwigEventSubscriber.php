1bb395fdddece7826a3c2dc8bc7fe3de9f44c06b
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:11:41 +0100

    Add 12.3: Sorting Conferences by Year and City

---
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Repository\ConferenceRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Twig\Environment;
+
+class TwigEventSubscriber implements EventSubscriberInterface
+{
+    private $twig;
+    private $conferenceRepository;
+
+    public function __construct(Environment $twig, ConferenceRepository $conferenceRepository)
+    {
+        $this->twig = $twig;
+        $this->conferenceRepository = $conferenceRepository;
+    }
+
+    public function onControllerEvent(ControllerEvent $event)
+    {
+        $this->twig->addGlobal('conferences', $this->conferenceRepository->findAll());
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ControllerEvent::class => 'onControllerEvent',
+        ];
+    }
+}
