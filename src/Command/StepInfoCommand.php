0fd5f9111de85dc6015b40f3175f6c83a6bffd79
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:19:52 +0100

    Add 21.6: Profiling and Comparing Performance

---
+++ src/Command/StepInfoCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Contracts\Cache\CacheInterface;
+
+class StepInfoCommand extends Command
+{
+    protected static $defaultName = 'app:step:info';
+
+    private $cache;
+
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $step = $this->cache->get('app.current_step', function ($item) {
+            $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
+            $process->mustRun();
+            $item->expiresAfter(30);
+
+            return $process->getOutput();
+        });
+        $output->writeln($step);
+
+        return 0;
+    }
+}
