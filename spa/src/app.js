cd89552d1ee1b17b9de737857c04d8b96f96ad63
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:23:52 +0100

    Add 27.2: Running an SPA in the Browser

---
+++ spa/src/app.js
@@ -0,0 +1,11 @@
+import {h, render} from 'preact';
+
+function App() {
+    return (
+        <div>
+            Hello world!
+        </div>
+    )
+}
+
+render(<App />, document.getElementById('app'));
