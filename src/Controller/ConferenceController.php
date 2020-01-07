4f4acfd345845f0e3a6ccbfd5415a16a34b6063c
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:12:55 +0100

    Add 13.6: Using Slugs in the Application

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -31,7 +31,7 @@ class ConferenceController extends AbstractController
     }
 
     /**
-     * @Route("/conference/{id}", name="conference")
+     * @Route("/conference/{slug}", name="conference")
      */
     public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
     {
