9b7645440bd497f819fdae0e9e9a317d8658db77
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:07:55 +0100

    Add 5.1: Understanding Symfony Environments

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
