ccb708622f46b5f03f6da6be5017500e6aec55f8
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:12:55 +0100

    Add 13.3: Defining a Complex Lifecycle Callback

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ConferenceRepository")
@@ -58,6 +59,13 @@ class Conference
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string) $slugger->slug((string) $this)->lower();
+        }
     }
 
     public function getCity(): ?string
