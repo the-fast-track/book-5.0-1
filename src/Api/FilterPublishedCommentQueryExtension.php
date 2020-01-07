6ada20862866258752a03f28a619394423d92ccc
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:23:17 +0100

    Add 26.4: Configuring CORS

---
+++ src/Api/FilterPublishedCommentQueryExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Api;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Comment;
+use Doctrine\ORM\QueryBuilder;
+
+class FilterPublishedCommentQueryExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    public function applyToCollection(QueryBuilder $qb, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    {
+        if (Comment::class === $resourceClass) {
+            $qb->andWhere(sprintf("%s.state = 'published'", $qb->getRootAliases()[0]));
+        }
+    }
+
+    public function applyToItem(QueryBuilder $qb, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
+    {
+        if (Comment::class === $resourceClass) {
+            $qb->andWhere(sprintf("%s.state = 'published'", $qb->getRootAliases()[0]));
+        }
+    }
+}
