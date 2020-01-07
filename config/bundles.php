a748c7a3bb70cd149960c6d52ded99b22b88fe83
Fabien Potencier <fabien@potencier.org>
2020-01-07 09:20:21 +0100

    Add 22.0: Using Sass

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
